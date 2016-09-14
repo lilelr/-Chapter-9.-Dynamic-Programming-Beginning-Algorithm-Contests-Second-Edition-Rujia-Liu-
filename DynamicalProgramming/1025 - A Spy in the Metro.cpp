6b323f98adece94c36a902eb75ca8dcbec5b4865
lilelr <lilelr@163.com>
2016-09-14 21:19:46 +0800

    1025 - A Spy in the Metro

---
+++ DynamicalProgramming/1025 - A Spy in the Metro.cpp
@@ -0,0 +1,93 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <string>
+#include <vector>
+using namespace std;
+#define LL long long
+const int maxn = 55;
+const int maxt = 205;
+const int INF = 1000000000;
+
+int mtime[maxn];  // n-1 个数，表示从i到i+1车站的所需时间，1<=i<=n-1
+int has_train[maxt][maxn][2];  // has_train[i][j][k]表示在状态
+// 第i时刻在j车站是否有往左出发的车辆(k=1)；k=0则是往右出发的车
+int dp[maxt]
+      [maxn];  // 状态定义，表示i时刻在车站j所需等待的最少时间,i,j都是按序变化的
+
+int main() {
+  //  freopen(
+  //      "/Users/yuxiao/XcodeProject/DynamicalProgramming/DynamicalProgramming/in",
+  //      "r", stdin);
+  //  freopen(
+  //      "/Users/yuxiao/XcodeProject/DynamicalProgramming/DynamicalProgramming/"
+  //      "out",
+  //      "w", stdout);
+
+  int T, n, kcase;
+  //  cin >> T;
+  kcase = 1;
+  while (cin >> n >> T && n) {
+    int M1, M2, d;
+    for (int i = 1; i <= n - 1; i++) {
+      cin >> mtime[i];
+    }
+
+    memset(has_train, 0, sizeof(has_train));
+    cin >> M1;  //输入从车站1出发的车辆个数
+    while (M1--) {
+      cin >> d;
+      //计算某一辆从车站1出发的车到达其余各站的时间
+      for (int j = 1; j <= n - 1; j++) {
+        if (d <= T) {
+          has_train[d][j][0] = 1;
+        }
+        d += mtime[j];
+      }
+    }
+
+    cin >> M2;  //输入从车站n出发的车辆个数
+    while (M2--) {
+      cin >> d;
+      //计算某一辆从车站n出发的车到达其余各站的时间
+      for (int j = n - 1; j >= 1; j--) {
+        if (d <= T) {
+          has_train[d][j + 1][1] = 1;
+        }
+        d += mtime[j];
+      }
+    }
+
+    //边界条件
+    for (int i = 1; i <= n - 1; i++) {
+      dp[T][i] = INF;  //初始化等待时间为无穷大
+    }
+    dp[T][n] = 0;  //由于在T时刻n车站与间谍会面，因此等待时间为0
+
+    for (int i = T - 1; i >= 0; i--) {
+      for (int j = 1; j <= n; j++) {
+        dp[i][j] = dp[i + 1][j] + 1;  //等待一个时间单位
+        if (j < n && has_train[i][j][0] && i + mtime[j] <= T) {
+          dp[i][j] = min(dp[i][j], dp[i + mtime[j]][j + 1]);  //右
+        }
+
+        if (j > 1 && has_train[i][j][1] && i + mtime[j - 1] <= T) {
+          dp[i][j] = min(dp[i][j], dp[i + mtime[j - 1]][j - 1]);  //左
+        }
+      }
+    }
+    cout << "Case Number " << kcase++ << ": ";
+    if (dp[0][1] >= INF) {
+      cout << "impossible\n";
+    } else {
+      cout << dp[0][1] << endl;
+    }
+  }
+  return 0;
+}
--- DynamicalProgramming/DAG_coins.cpp
+++ DynamicalProgramming/DAG_coins.cpp
@@ -1,90 +1,90 @@
-//动态规划 递推法求硬币集合问题
-#include <algorithm>
-#include <cmath>
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <iostream>
-#include <map>
-#include <queue>
-#include <set>
-#include <string>
-#include <vector>
-using namespace std;
-#define LL long long
-
-////input
-// 1
-// 3
-// 1 5 10
-// 20
-
-const int maxn = 100;
-const int INF = (1 << 30);
-
-int min_coins[maxn],
-    path_min_coins[maxn];  //所需最小的硬币数量，以及记录硬币的序号的path数组
-int max_coins[maxn], path_max_coins[maxn];
-int coins[maxn];  //银币值
-int sum;          //所需满足的硬币之和
-int n;            //共n枚硬币
-
-//打印所需硬币的序号
-void print_ans(int* path, int sum) {
-  while (sum) {
-    cout << path[sum] << " ";
-    sum -= coins[path[sum]];
-  }
-}
-
-int main() {
-  freopen(
-      "/Users/yuxiao/XcodeProject/DynamicalProgramming/DynamicalProgramming/in",
-      "r", stdin);
-  freopen(
-      "/Users/yuxiao/XcodeProject/DynamicalProgramming/DynamicalProgramming/"
-      "out",
-      "w", stdout);
-
-  int T, t;
-  cin >> T;
-  t = 1;
-  while (T--) {
-    cin >> n;
-    for (int i = 0; i < n; i++) {
-      cin >> coins[i];
-    }
-    cin >> sum;
-    for (int i = 1; i <= sum; i++) {
-      min_coins[i] = INF;
-      max_coins[i] = -INF;
-    }
-    min_coins[0] = 0;
-    max_coins[0] = 0;
-
-    for (int i = 1; i <= sum; i++) {
-      for (int j = 0; j < n; j++) {
-        if (i >= coins[j]) {
-          //当min 值相同时，取最小的i值，故判断不用 “>="
-          if (min_coins[i] > min_coins[i - coins[j]] + 1) {
-            min_coins[i] = min_coins[i - coins[j]] + 1;
-            path_min_coins[i] = j;
-          }
-
-          if (max_coins[i] < max_coins[i - coins[j]] + 1) {
-            max_coins[i] = max_coins[i - coins[j]] + 1;
-            path_max_coins[i] = j;
-          }
-        }
-      }
-    }
-    printf("Case #%d:\n", t++);
-    cout << "min:  " << min_coins[sum] << endl;
-    print_ans(path_min_coins, sum);
-    cout << endl;
-    cout << "max:  " << max_coins[sum] << endl;
-    print_ans(path_max_coins, sum);
-    cout << endl;
-  }
-  return 0;
-}
+////动态规划 递推法求硬币集合问题
+//#include <algorithm>
+//#include <cmath>
+//#include <cstdio>
+//#include <cstdlib>
+//#include <cstring>
+//#include <iostream>
+//#include <map>
+//#include <queue>
+//#include <set>
+//#include <string>
+//#include <vector>
+// using namespace std;
+//#define LL long long
+//
+//////input
+//// 1
+//// 3
+//// 1 5 10
+//// 20
+//
+// const int maxn = 100;
+// const int INF = (1 << 30);
+//
+// int min_coins[maxn],
+//    path_min_coins[maxn];  //所需最小的硬币数量，以及记录硬币的序号的path数组
+// int max_coins[maxn], path_max_coins[maxn];
+// int coins[maxn];  //银币值
+// int sum;          //所需满足的硬币之和
+// int n;            //共n枚硬币
+//
+////打印所需硬币的序号
+// void print_ans(int* path, int sum) {
+//  while (sum) {
+//    cout << path[sum] << " ";
+//    sum -= coins[path[sum]];
+//  }
+//}
+//
+// int main() {
+//  freopen(
+//      "/Users/yuxiao/XcodeProject/DynamicalProgramming/DynamicalProgramming/in",
+//      "r", stdin);
+//  freopen(
+//      "/Users/yuxiao/XcodeProject/DynamicalProgramming/DynamicalProgramming/"
+//      "out",
+//      "w", stdout);
+//
+//  int T, t;
+//  cin >> T;
+//  t = 1;
+//  while (T--) {
+//    cin >> n;
+//    for (int i = 0; i < n; i++) {
+//      cin >> coins[i];
+//    }
+//    cin >> sum;
+//    for (int i = 1; i <= sum; i++) {
+//      min_coins[i] = INF;
+//      max_coins[i] = -INF;
+//    }
+//    min_coins[0] = 0;
+//    max_coins[0] = 0;
+//
+//    for (int i = 1; i <= sum; i++) {
+//      for (int j = 0; j < n; j++) {
+//        if (i >= coins[j]) {
+//          //当min 值相同时，取最小的i值，故判断不用 “>="
+//          if (min_coins[i] > min_coins[i - coins[j]] + 1) {
+//            min_coins[i] = min_coins[i - coins[j]] + 1;
+//            path_min_coins[i] = j;
+//          }
+//
+//          if (max_coins[i] < max_coins[i - coins[j]] + 1) {
+//            max_coins[i] = max_coins[i - coins[j]] + 1;
+//            path_max_coins[i] = j;
+//          }
+//        }
+//      }
+//    }
+//    printf("Case #%d:\n", t++);
+//    cout << "min:  " << min_coins[sum] << endl;
+//    print_ans(path_min_coins, sum);
+//    cout << endl;
+//    cout << "max:  " << max_coins[sum] << endl;
+//    print_ans(path_max_coins, sum);
+//    cout << endl;
+//  }
+//  return 0;
+//}
