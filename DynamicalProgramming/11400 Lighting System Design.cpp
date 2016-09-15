20fd526af108a0defcb19032963754cb7a71782f
lilelr <lilelr@163.com>
2016-09-15 22:11:04 +0800

    11584 - Partitioning by Palindromes

--- DynamicalProgramming/11400 Lighting System Design.cpp
+++ DynamicalProgramming/11400 Lighting System Design.cpp
@@ -1,62 +1,64 @@
-// https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&category=847&problem=2395&mosmsg=Submission+received+with+ID+18004148
-// O(n^2)
-#include <algorithm>
-#include <cmath>
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <iostream>
-#include <map>
-#include <queue>
-#include <set>
-#include <string>
-#include <vector>
-using namespace std;
-#define LL long long
-const int maxn = 1005;
-struct lamp {
-  int voltage;  //电压
-  int cost;     //该类型灯泡的价格
-  int k;        //该类型灯泡的电源价格
-  int number;   //所需该类型灯泡的数量
-  //按电压从小到大排序的比较函数
-  bool operator<(const lamp& b) const { return voltage < b.voltage; }
-} lamp[maxn];
-
-int n;
-int sum[maxn];
-// d[i] 表示购买前i种灯泡所需的最小费用，包括灯泡的价格*数量，以及电源费用
-int d[maxn];
-int main() {
-  //  freopen(
-  //      "/Users/yuxiao/XcodeProject/DynamicalProgramming/DynamicalProgramming/in",
-  //      "r", stdin);
-  //  freopen(
-  //      "/Users/yuxiao/XcodeProject/DynamicalProgramming/DynamicalProgramming/"
-  //      "out",
-  //      "w", stdout);
-
-  while (cin >> n && n) {
-    for (int i = 1; i <= n; i++) {
-      cin >> lamp[i].voltage >> lamp[i].k >> lamp[i].cost >> lamp[i].number;
-    }
-    //按电压从小到大排序
-    sort(lamp + 1, lamp + n + 1);
-    sum[0] = 0;
-    for (int i = 1; i <= n; i++) {
-      sum[i] = sum[i - 1] + lamp[i].number;
-    }
-    d[0] = 0;
-    for (int i = 1; i <= n; i++) {
-      //// 前i个灯泡全买类型i
-      d[i] = sum[i] * lamp[i].cost + lamp[i].k;  //初始化
-      for (int j = 1; j <= i; j++) {
-        // 第 j+1 到i 共( sum[i] - sum[j] ) 个灯泡全买类型i
-        d[i] = min(d[i], d[j] + (sum[i] - sum[j]) * lamp[i].cost + lamp[i].k);
-      }
-    }
-    cout << d[n] << "\n";
-  }
-
-  return 0;
-}
+////
+///https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&category=847&problem=2395&mosmsg=Submission+received+with+ID+18004148
+//// O(n^2)
+//#include <algorithm>
+//#include <cmath>
+//#include <cstdio>
+//#include <cstdlib>
+//#include <cstring>
+//#include <iostream>
+//#include <map>
+//#include <queue>
+//#include <set>
+//#include <string>
+//#include <vector>
+// using namespace std;
+//#define LL long long
+// const int maxn = 1005;
+// struct lamp {
+//  int voltage;  //电压
+//  int cost;     //该类型灯泡的价格
+//  int k;        //该类型灯泡的电源价格
+//  int number;   //所需该类型灯泡的数量
+//  //按电压从小到大排序的比较函数
+//  bool operator<(const lamp& b) const { return voltage < b.voltage; }
+//} lamp[maxn];
+//
+// int n;
+// int sum[maxn];
+//// d[i] 表示购买前i种灯泡所需的最小费用，包括灯泡的价格*数量，以及电源费用
+// int d[maxn];
+// int main() {
+//  //  freopen(
+//  //
+//  "/Users/yuxiao/XcodeProject/DynamicalProgramming/DynamicalProgramming/in",
+//  //      "r", stdin);
+//  //  freopen(
+//  // "/Users/yuxiao/XcodeProject/DynamicalProgramming/DynamicalProgramming/"
+//  //      "out",
+//  //      "w", stdout);
+//
+//  while (cin >> n && n) {
+//    for (int i = 1; i <= n; i++) {
+//      cin >> lamp[i].voltage >> lamp[i].k >> lamp[i].cost >> lamp[i].number;
+//    }
+//    //按电压从小到大排序
+//    sort(lamp + 1, lamp + n + 1);
+//    sum[0] = 0;
+//    for (int i = 1; i <= n; i++) {
+//      sum[i] = sum[i - 1] + lamp[i].number;
+//    }
+//    d[0] = 0;
+//    for (int i = 1; i <= n; i++) {
+//      //// 前i个灯泡全买类型i
+//      d[i] = sum[i] * lamp[i].cost + lamp[i].k;  //初始化
+//      for (int j = 1; j <= i; j++) {
+//        // 第 j+1 到i 共( sum[i] - sum[j] ) 个灯泡全买类型i
+//        d[i] = min(d[i], d[j] + (sum[i] - sum[j]) * lamp[i].cost + lamp[i].k);
+//      }
+//    }
+//    cout << d[n] << "\n";
+//  }
+//
+//  return 0;
+//}
---
+++ DynamicalProgramming/11584 - Partitioning by Palindromes.cpp
@@ -0,0 +1,65 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <string>
+#include <vector>
+using namespace std;
+#define LL long long
+const int maxn = 1005;
+int n, case_index, vistied[maxn][maxn], p[maxn][maxn], d[maxn];
+char s[maxn];
+
+// 递归判断回文，并记录
+int is_palindrome(int i, int j) {
+  if (i >= j) {
+    return 1;
+  }
+  if (s[j] != s[i]) {
+    return 0;
+  }
+  if (vistied[i][j] == case_index) {
+    return p[i][j];
+  }
+
+  vistied[i][j] = case_index;
+  p[i][j] = is_palindrome(i + 1, j - 1);
+  return p[i][j];
+}
+
+int main() {
+  //  freopen(
+  //      "/Users/yuxiao/XcodeProject/DynamicalProgramming/DynamicalProgramming/in",
+  //      "r", stdin);
+  //  freopen(
+  //      "/Users/yuxiao/XcodeProject/DynamicalProgramming/DynamicalProgramming/"
+  //      "out",
+  //      "w", stdout);
+
+  int T, t;
+  cin >> T;
+  t = 1;
+  memset(vistied, 0, sizeof(vistied));
+  case_index = 1;
+  while (T--) {
+    scanf("%s", s + 1);  // s[0] = "\0"
+    n = strlen(s + 1);
+    d[0] = 0;
+    for (int i = 1; i <= n; i++) {
+      d[i] = i + 1;
+      for (int j = 0; j < i; j++) {
+        if (is_palindrome(j + 1, i)) {
+          d[i] = min(d[i], d[j] + 1);
+        }
+      }
+    }
+    printf("%d\n", d[n]);
+    case_index++;
+  }
+  return 0;
+}
